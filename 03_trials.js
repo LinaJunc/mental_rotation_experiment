a23b75e78269a0f593f54c2d891064ef639a6141
LinaJunc <lina.junc@gmail.com>
2019-05-15 18:17:49 +0200

    Upload _babe files and folders

---
+++ 03_trials.js
@@ -0,0 +1,62 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+  key_press_trials: [
+  {
+      question: "Are the objects same or different?",
+      picture: 'mental_rotation_images/1_50_same.jpg',
+      key1: 'f',
+      key2: 'j',
+      f: 'same',
+      j: 'different',
+      expected: 'same',
+      pictureID: '1',
+      degree: '50'
+  },
+  {
+      question: "Are the objects same or different?",
+      picture: 'mental_rotation_images/7_150_different.jpg',
+      key1: 'f',
+      key2: 'j',
+      f: 'same',
+      j: 'different',
+      expected: 'different',
+      pictureID: '7',
+      degree: '150'
+  },
+  {
+      question: "Are the objects same or different?",
+      picture: 'mental_rotation_images/4_50_same.jpg',
+      key1: 'f',
+      key2: 'j',
+      f: 'same',
+      j: 'different',
+      expected: 'same',
+      pictureID: '4',
+      degree: '50'
+  },
+  {
+      question: "Are the objects same or different?",
+      picture: 'mental_rotation_images/8_150_same.jpg',
+      key1: 'f',
+      key2: 'j',
+      f: 'same',
+      j: 'different',
+      expected: 'same',
+      pictureID: '8',
+      degree: '150'
+  },
+  {
+      question: "Are the objects same or different?",
+      picture: 'mental_rotation_images/3_150_different.jpg',
+      key1: 'f',
+      key2: 'j',
+      f: 'same',
+      j: 'different',
+      expected: 'different',
+      pictureID: '3',
+      degree: '150'
+  },
+  ]
+};
